decccda2ff2a10f7917c807e4f0850e009343c0e
erick <erickgomez@mymail.mines.edu>
2021-01-18 21:14:27 -0700

    Finished a styling background images, styling a navbar, and some parsing stuff

--- apps/inventory/urls.py
+++ apps/inventory/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='inventory_index'),
     path('type<type_id>/', views.type, name='type'),
     path('<item_id>/', views.item, name='item')
 ]
--- apps/uploads/urls.py
+++ apps/uploads/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='upload_index'),
     path('inventory/', views.inventory, name = 'inventory'),
     path('components/', views.components, name = 'components')
 ]
